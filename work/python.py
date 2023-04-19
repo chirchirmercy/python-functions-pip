5e040cc42446bb3cbdb338c32655f18f3fefe40b
chirchirmercy <cmercychptoo.com>
2023-04-19 20:33:16 +0300

    first commit

---
+++ work/python.py
@@ -0,0 +1,67 @@
+
+# functions are created using def keywords
+# functions must be invoked  to bring into action
+# once the function have defined it can be called any/several number of times
+def greet():
+    print("Hello")
+    print("Wow! How are you")
+
+greet()
+
+
+
+# //Arguments can be passed to the functions
+def greet(name):
+    print("Hello",name)
+    print("How do you do")
+
+    greet("mercy")
+
+
+    # //multiple arguments can be passed through the function
+
+    def add_numbers(num1,num2):
+        result= n1+n2
+        print("The sum is",result)
+
+
+        number1=5.4
+        number2=6.8
+
+        add_numbers(number1 + number2)
+
+
+# //inbuild functions are defined inside function
+# find the length of the list 
+marks=[10,20,70,90]
+length=len(marks)
+                         
+print("lenngth is",length)
+
+
+# //output=5
+
+# print the total sum of marks
+marks_sum=sum(marks)
+print("total marks is",marks_sum)
+
+# output = total marks is 190
+
+# find the average marks
+
+def average-marks(marks):
+    sum _of _marks =sum(marks)
+    total_ subjects=len(marks)
+    average-marks=sum_of_marks/total_subjects
+
+    return average-marks
+    marks=[20,30,40,50]
+
+    average-marks=(average-marks(marks))
+    print("your average marks is",average_marks)
+
+
+
+
+
+
